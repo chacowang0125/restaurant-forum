108dad7e3c04f5be703c4a3b2772db2180ee9783
chaco <chacowang0125@gmail.com>
2021-11-30 01:33:26 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+# 部署錢打包push到github上
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f https://github.com/<USERNAME>/<USERNAME>.GitHub.io.git main
+
+# if you are deploying to https://<USERNAME>.Github.io/<REPO>
+# git push -f https://github.com/<USERNAME>/<REPO>.git main:gh-pages
+git push -f https://github.com/chacowang0125/restaurant-forum.git main:gh-pages
